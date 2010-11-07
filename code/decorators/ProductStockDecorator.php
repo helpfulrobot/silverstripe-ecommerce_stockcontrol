8ca8c1a3e7894836d8d08e0ef33833bb4493f14e
Jezait <Jezait@950b781c-c544-efab-2f99-90083ee69683>
2010-11-07 23:32:08 +0000

    simplified the stock control
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_stockcontrol/branches/simple@300 950b781c-c544-efab-2f99-90083ee69683

--- code/decorators/ProductStockDecorator.php
+++ code/decorators/ProductStockDecorator.php
@@ -2,69 +2,64 @@
 
 /**
  * ProductStockDecorator
- * Extension of Product for adding stock level capabilities.
+ * Extension of Product for storing current stock level.
  */
 
 class ProductStockDecorator extends DataObjectDecorator{
 	
+	function extraStatics(){
+		return array(
+			'db' => array(
+				'Stock' => 'Int'
+			)
+		);
+	}
+
 	/*
 	 * Allow setting stock level in CMS
 	 */
 	function updateCMSFields(&$fields){
-		
-		//FIXME: hack, because $this->Variations doesn't seem to work???
-		if(DataObject::get('ProductVariation',"\"ProductID\" = ".$this->owner->ID) == null){ 
+		if(!$this->owner->Variations()){ 
 			$fields->addFieldToTab('Root.Content.Main',new NumericField('Stock','Stock'),'Content');
 		}
-		
-		
 	}
 	
-	/*
-	 * Getter for stock level
-	 */
-	function getStock(){
-		return ProductStockCalculatedQuantity::get_quantity_by_product_id($this->owner->ID);
-	}
-	
-	/*
-	 * Setter for stock level
-	 */
-	function setStock($value){
-		if(!$this->owner->ID){
-			$this->owner->TempStockValueToBeSaved = $value; //store for saving later (see onAfterWrite below)
-		}
-		
-		//only set stock level if it differs from previous
-		if($this->owner->ID && $value != $this->owner->getStock() && $member = Member::currentUser()){
-			$parent = ProductStockCalculatedQuantity::get_by_product_id($this->owner->ID);
-			$obj = new ProductStockManualUpdate();
-			$obj->ParentID = $parent->ID;
-			$obj->Quantity = (int)$value;
-			$obj->MemberID = $member->ID;
-			$obj->write();
-		}
-	}
-	
-	/*
-	 * Catch the case where stock level has been set on a new Product DataObject.
-	 */
-	function onAfterWrite(){
-		if($this->owner->TempStockValueToBeSaved){
-			$this->owner->Stock = $this->owner->TempStockValueToBeSaved;
-			$this->owner->TempStockValueToBeSaved = null;
-		}
-	}
-	
-	/*
+	/**
 	 * Only allow purchase if stock levels allow
-	 * TODO: customise this to a certian stock level, on, or off
 	 */
 	function canPurchase(){
+		//TODO: customise this to a certian stock level, on, or off
 		if($this->owner->Stock <= 0){
 			 return false;
 		}
 		return null; //returning null ensures that can checks continue
 	}
+	
+	function decrementStock($qty = 1,$write = true){
+		$this->owner->Stock = $this->owner->Stock - $qty;
+		if($this->owner->Stock < 0)
+			$this->owner->Stock = 0;
+			
+		//save & publish new stock level
+		if($write){
+			$this->owner->writeToStage('Stage');
+			$this->owner->publish('Stage','Live');
+		}
+	}
+	
+	/**
+	 * Get the stock level for this product's variations;
+	 */
+	 //TODO: could replace this with a SUM query
+	function VariationStock(){
+		$stock = 0;
+		if($vars = $this->owner->Variations()){
+			foreach($vars as $var){
+				$stock += $var->Stock;
+			}
+		}
+		return $stock;
+	}
+
 	
 }
